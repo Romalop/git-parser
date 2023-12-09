3ff4b99ffc755839a67c7dd4129dbe1d7f8dc86f
Romalop <roman.reutskiy@gmail.com>
2023-12-09 13:44:50 +1000

    Весь скрипт переделан в функции и выполнена проверка на качество фотографии. Выбора качества еще нет. Реализованна запись ссылок в файл, сохранение файла в папке Gorskiy. Скрипт отрабатывает за 1,5 часа

--- index.php
+++ index.php
@@ -1,82 +1,92 @@
 <?php
-    /*Создаем массив с ссылками на страницы выставок с фотографиями*/
-    $url = 'https://www.loc.gov/exhibits/empire/';
-    
-    //Получаем имя страниц выставки
-    $html = file_get_contents($url);
-    // Создаем объект DOMDocument
-    $dom = new DOMDocument;
-    // Игнорировать предупреждения и ошибки
-    libxml_use_internal_errors(true);
-    //Загружаем HTML-код
-    $dom->loadHTML($html);
+    function collector($url){
+        //Запоминаем текущее время до выполнения скрипта
+        $start_time = microtime(true);
 
-    //Получаем все элементы <a> внутри <div> с id="tert_nav"
-    $divElement = $dom->getElementById('tert_nav');
-    $links = $divElement->getElementsByTagName('a');
+        /*Создаем массив с ссылками на странице выставок с фотографиями*/
 
-    //Инициализируем пустой массив для имен страниц
-    $linkPage = [];
-
-    //Обрабатываем каждую найденную ссылку и добавляем имена старниц в массив
-    foreach ($links as $link) {
-        // Получаем атрибут href и добавляем его в массив
-        $linkPage[] = $link->getAttribute('href');
-    }
-    
-    //Получаем ссылки на страниц выставок и добавляем в массив//
-    foreach($linkPage as $value){
-        if (str_ends_with($value, ".html")){
-            $urlExhibitions[] = $url.$value;
-        }
-    }
-    
-    /*Переходим по страницам и собираем все ссылки на картинки в один большой массив*/
-
-    foreach ($urlExhibitions as $value){
-        $html = file_get_contents($value);
-        $dom = new DOMDocument; 
+        //Получаем имя каталога фото с пагинацией
+        $html = file_get_contents($url);
+        //Создаем объект DOMDocument
+        $dom = new DOMDocument;
+        //Игнорируем предупреждения и ошибки
+        libxml_use_internal_errors(true);
         $dom->loadHTML($html);
+
         $xpath = new DOMXPath($dom);
 
-        //Используем XPath-выражение для выбора элемента с классом pic-box-alt w-borderr
-        $element = $xpath->query('//div[@class="pic-box-alt w-border"]/a');
+        //Получаем количество элементов в данном каталоге, ятобы далее расчитать количество страниц для цикла перебора
+        $countElement = $xpath->query('//span[@class="count"]')->item(0);
+        $countText = str_replace(',','',$countElement->textContent);
+        $countPage = (intval($countText/25))+1;
 
-        foreach ($element as $value){
-            $linkImg[] = $url.$value->getAttribute('href');
+        //Цикл для прохода по пагинации и собирация всех ссылок на картинки в массив
+        $domPage = new DOMDocument;
+        for ($i=1; $i <= $countPage; $i++){
+            sleep(5);
+            $urlPage = $url.'&sp='.$i;
+            $htmlPage = file_get_contents($urlPage);
+            echo $i;
+            $domPage->loadHtml($htmlPage);
+            $xpathPage = new DOMXPath($domPage);
+            $urlPic = $xpathPage->query('//img[@class="iconic "]');
+            foreach ($urlPic as $value){
+                $serviceUrl = strstr($value->getAttribute('src'), '_', true).'u.tif';
+                if (str_ends_with($serviceUrl, '.tif')){
+                    $linkImg[] = str_replace('/service/', '/master/', $serviceUrl);
+                }else{
+                    $linkImg[] = $serviceUrl;
+                }
+                sleep(2);
+            }
         }
-    }
-    print_r($linkImg);
+        return $linkImg;
 
-    /*Скачиваем изображения и сохраняем их в папку Downloads*/
-
-    // Папка, в которую вы хотите сохранить изображения
-    $downloadFolder = '/Users/roman/Downloads/';
-
-    // Проверяем, существует ли папка Download, и создаем ее, если нет
-    /*if (!is_dir($downloadFolder)) {
-        mkdir($downloadFolder, 0755, true);
-    }*/
-
-    // Итерируем по ссылкам и скачиваем файлы
-    foreach ($linkImg as $index => $value) {
-        // Получаем имя файла из URL
-        $filename = $downloadFolder . 'image' . $index . '.jpg';
-
-        // Скачиваем файл и сохраняем его
-        $fileContent = file_get_contents($value);
-        file_put_contents($filename, $fileContent);
-
-        echo "Файл $filename успешно скачан.\n";
-        sleep(10);
     }
 
-    echo "Все файлы успешно скачаны в папку Download.\n";
-
+    function createFileLink($linkImg){
+        $line = implode(',', $linkImg);
+        //Записываем текст в файл, если файл не сушествует он будет создан
+        file_put_contents('/Users/roman/Downloads/link_photo.txt', $line);
+        print_r($linkImg);
+    }
     
+    function parser($url){
+        $start_time = microtime(true);
+        $linkImg = collector($url);
+        createFileLink($linkImg);
+        $end_time = microtime(true);
+        $execution_time = $end_time - $start_time;
+        echo "Время выполнения скрипта: $execution_time секунд";
 
-   
-    
-    
+    }
 
+    function downloadPhoto(){
+
+    }
+    parser('https://www.loc.gov/collections/prokudin-gorskii/?fa=original-format:photo,+print,+drawing');
+
+    // /*Скачиваем изображения и сохраняем их в папку Downloads*/
+
+    // // Папка, в которую вы хотите сохранить изображения
+    // $downloadFolder = '/Users/roman/Downloads/Gorskiy/';
+
+    // // Итерируем по ссылкам и скачиваем файлы
+    // foreach ($linkImg as $index => $value) {
+    //     // Получаем имя файла из URL
+    //     $filename = $downloadFolder . 'image' . $index . '.jpg';
+
+    //     // Скачиваем файл и сохраняем его
+    //     $fileContent = file_get_contents($value);
+    //     file_put_contents($filename, $fileContent);
+
+    //     echo "Файл $filename успешно скачан.\n";
+    //     sleep(2);
+    // }
+
+    // echo "Все файлы успешно скачаны в папку Gorskiy.\n";
+
+//r.jpg
+//v.jpg
+//u.tif
 ?>

28027114484c8caf803ab37475fa2c3909d046ad
Romalop <roman.reutskiy@gmail.com>
2024-01-09 23:39:57 +1000

    В коде есть 2 функции parser() и downloadStart(). Первая скачивает ссылки, а вторая скачивает картинки, запускать их лучше всего по отдельности

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         $dom->loadHTML($html);
 
         $xpath = new DOMXPath($dom);
-        $y = 1;
+        
         //Получаем количество элементов в данном каталоге, ятобы далее расчитать количество страниц для цикла перебора
         $countElement = $xpath->query('//span[@class="count"]')->item(0);
         $countText = str_replace(',','',$countElement->textContent);
@@ -19,8 +19,9 @@
 
         //Цикл для прохода по пагинации и собирация всех ссылок на картинки в массив
         $domPage = new DOMDocument;
-        for ($i=1; $i <= $y; $i++){
-            sleep(5);
+        //$y = 1;
+        for ($i = 1; $i <= $countPage; $i++){
+            sleep(2);
             $urlPage = $url.'&sp='.$i;
             $htmlPage = file_get_contents($urlPage);
             echo $i;
@@ -28,59 +29,71 @@
             $xpathPage = new DOMXPath($domPage);
             $urlPic = $xpathPage->query('//img[@class="iconic "]');
             foreach ($urlPic as $value){
-                $serviceUrl = strstr($value->getAttribute('src'), '_', true).'r.jpg';
-                if (str_ends_with($serviceUrl, '.tif')){
-                    $linkImg[] = str_replace('/service/', '/master/', $serviceUrl);
+                if (str_contains($value->getAttribute('src'), 't.gif') == true){
+                    $serviceUrl = strstr($value->getAttribute('src'), 't.gif', true).'u.tif';
+                    if (str_ends_with($serviceUrl, '.tif')){
+                        $linkImg[] = str_replace('/service/', '/master/', $serviceUrl);
+                    }else{
+                        $linkImg[] = $serviceUrl;
+                    }
+                    sleep(2);
                 }else{
-                    $linkImg[] = $serviceUrl;
+                    $serviceUrl = strstr($value->getAttribute('src'), '_', true).'u.tif';
+                    if (str_ends_with($serviceUrl, '.tif')){
+                        $linkImg[] = str_replace('/service/', '/master/', $serviceUrl);
+                    }else{
+                        $linkImg[] = $serviceUrl;
+                    }
+                    sleep(2);
                 }
-                sleep(3);
+                
             }
         }
         return $linkImg;
-
     }
 
     function createFileLink($linkImg){
         $line = implode(',', $linkImg);
         //Записываем текст в файл, если файл не сушествует он будет создан
-        file_put_contents('/Users/roman/Downloads/Gorskiy/link_photo.txt', $line);
+        file_put_contents('link_photo.txt', $line);
         print_r($linkImg);
     }
     function downloadPhoto(){
-        // $limit = 2;
+
         // Папка, в которую вы хотите сохранить изображения
-        $start_time = microtime(true);
-        $downloadFolder = '/Users/roman/Downloads/Gorskiy/';
-        $filePath = '/Users/roman/Downloads/Gorskiy/link_photo.txt';
+        $downloadFolder = 'img/';
+        $filePath = 'link_photo.txt';
         $fileName = file_get_contents($filePath, true);
         $linkImg = explode(',', $fileName);
 
         foreach ($linkImg as $index => $value) {
-            // Получаем имя файла из URL
-            $filename = $downloadFolder . 'image' . $index . '.jpg';
-
-            // Скачиваем файл и сохраняем его
-            $fileContent = file_get_contents($value);
-            file_put_contents($filename, $fileContent);
-
-            
-
-            echo "Файл $filename успешно скачан.\n";
-            // // Увеличиваем счетчик обработанных элементов
-            // $limit--;
-
-            // // Проверяем, достигнут ли лимит
-            // if ($limit <= 0) {
-            //     break; // Прерываем цикл, если лимит достигнут
-            // }
-            sleep(2);
+                $start_time_photo = microtime(true);
+                // Получаем имя файла из URL
+                $filename = $downloadFolder . 'image' .'_'. $index . '.jpg';
+                $success = false;
+                
+                
+                //Запускаем цикл, который будет отлавливать ошибки, и пытаться снова запустить скачивание файла
+                while (!$success) {  
+                    // Скачиваем файл и сохраняем его
+                    $fileContent = @file_get_contents($value);
+                    if ($fileContent !== false){
+                        
+                        file_put_contents($filename, $fileContent);
+                        $end_time_photo = microtime(true);
+                        $execution_time_photo = $end_time_photo - $start_time_photo;
+                        echo "Файл $filename успешно скачан за ".round($execution_time_photo, 2)." секунд.\n";
+                        $success = true;
+                        // if ($execution_time_photo < 50){
+                        //     sleep(10);
+                        // }
+                    } else{
+                        echo "Ошибка при скачивании файла $filename\n";
+                        sleep(300);
+                    }
+                }
         }
-
         echo "Все файлы успешно скачаны в папку Gorskiy.\n";
-        $end_time = microtime(true);
-        $execution_time = $end_time - $start_time;
-        echo "Время выполнения скрипта: $execution_time секунд";
     }
     
     function parser($url){
@@ -89,11 +102,19 @@
         $end_time = microtime(true);
         $execution_time = $end_time - $start_time;
         echo "Время выполнения скрипта: $execution_time секунд";
+        
+    }
+    function downloadStart(){
+        $start_time = microtime(true);
         downloadPhoto();
+        $end_time = microtime(true);
+        $execution_time = $end_time - $start_time;
+        echo "Время скачивания всех фотографий: $execution_time секунд";
+        
     }
 
-    parser('https://www.loc.gov/collections/prokudin-gorskii/?fa=original-format:photo,+print,+drawing');
-
+    //parser('https://www.loc.gov/collections/prokudin-gorskii/?fa=original-format:photo,+print,+drawing');
+    downloadStart();
     
     
     
@@ -101,5 +122,8 @@
 //r.jpg
 //v.jpg
 //u.tif
-/* Путь к папке в сервере /var/www/html*/
+/*Путь к папке в сервере /var/www/html*/
+/*Путь к логам сервера /var/log/apache2/error.log*/
+/*Путь к жестаку в 160 гигов /mnt/sdc/www/*/
+
 ?>
